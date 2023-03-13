1aee72f307a47e5dd1bc4604bb7659f6a373eddb
Oraegbu Charles <oraegbucharlesc@gmail.com>
2023-03-13 09:41:47 +0100

    add comment

--- data_engineering/partial_function.py
+++ data_engineering/partial_function.py
@@ -5,5 +5,5 @@ import pandas as pd
 
 def long_crunching_task(df: pd.DataFrame, offset: int) -> pd.DataFrame:
     time.sleep(30)
-    df["C"] = df["A"] + df["B"] + offset
+    df["C"] = df["A"] + df["B"] + offset  # imaginary non-vectorizational operation >_<
     return df
